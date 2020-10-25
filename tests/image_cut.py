82f4b3ed88f378a6e5f815f5537d7559256cc1c9
Renan Nakazawa <renan.nakazawa0@gmail.com>
2020-10-25 18:21:11 -0300

    fix image path

--- tests/image_cut.py
+++ tests/image_cut.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('abc.jpg')
+image = cv2.imread('../resources/test.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
 
